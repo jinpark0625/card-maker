acf5cabcb289dac4aa7a943e05821b076c72f55a
jinpark0625@gmail.com <jinpark0625@gmail.com>
2021-04-05 20:52:35 +0900

    add editor and preview

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,23 @@
 import styles from "./app.module.css";
 import Login from "./components/login/login";
+import Maker from "./components/maker/maker";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App({ authService }) {
-  return <Login authService={authService} />;
+  return (
+    <div className={styles.main}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
