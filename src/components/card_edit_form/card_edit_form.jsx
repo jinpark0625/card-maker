9c997c112fe5b66638fcbcb23182548733852d5e
jinpark0625@gmail.com <jinpark0625@gmail.com>
2021-04-06 16:03:42 +0900

    handle state chagne on editor

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./card_edit_form.module.css";
 import Card from "../card/card";
 import Button from "../button/button";
 import ImageFileInput from "../image_file_input/image_file_input";
 
-const CardEditForm = ({ card }) => {
+const CardEditForm = ({ card, updateCard, deleteCard }) => {
   const {
     name,
     company,
@@ -16,43 +16,86 @@ const CardEditForm = ({ card }) => {
     fileURL,
   } = card;
 
-  const onSubmit = () => {};
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+  const fileRef = useRef();
+
+  //   const onDelete = (event) => {
+  //     event.preventDefault();
+  //     const clickedName = nameRef.current.value;
+  //     removeCard(clickedName);
+  //   };
+
+  const onChange = (event) => {
+    if (event.currentTarget === null) {
+      return;
+    }
+    event.preventDefault();
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  const onSubmit = () => {
+    deleteCard(card);
+  };
 
   return (
     <form className={styles.form}>
       <input
+        ref={nameRef}
         className={styles.input}
         type="text"
         name="name"
-        placeholder={name}
+        value={name}
+        onChange={onChange}
       />
       <input
+        ref={companyRef}
         className={styles.input}
         type="text"
         name="company"
-        placeholder={company}
+        value={company}
+        onChange={onChange}
       />
-      <select className={styles.select} name="theme" placeholder={theme}>
-        <option placeholder="light">light</option>
-        <option placeholder="dark">dark</option>
-        <option placeholder="colorful">colorful</option>
+      <select
+        ref={themeRef}
+        className={styles.select}
+        name="theme"
+        value={theme}
+        onChange={onChange}
+      >
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+        <option value="colorful">colorful</option>
       </select>
       <input
+        ref={titleRef}
         className={styles.input}
         type="text"
         name="title"
-        placeholder={title}
+        value={title}
+        onChange={onChange}
       />
       <input
+        ref={emailRef}
         className={styles.input}
         type="text"
         name="email"
-        placeholder={email}
+        value={email}
+        onChange={onChange}
       />
       <textarea
+        ref={messageRef}
         className={styles.textarea}
         name="message"
-        placeholder={message}
+        value={message}
+        onChange={onChange}
       />
       <div className={styles.fileInput}>
         <ImageFileInput />
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -8,8 +8,8 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Maker = ({ authService }) => {
   const history = useHistory();
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: "1",
       name: "jin",
       company: "google",
@@ -20,7 +20,7 @@ const Maker = ({ authService }) => {
       fileName: "jin",
       fileURL: "jin.png",
     },
-    {
+    2: {
       id: "2",
       name: "lelel",
       company: "google",
@@ -31,7 +31,7 @@ const Maker = ({ authService }) => {
       fileName: "jin",
       fileURL: null,
     },
-    {
+    3: {
       id: "3",
       name: "safda",
       company: "google",
@@ -42,7 +42,7 @@ const Maker = ({ authService }) => {
       fileName: "jin",
       fileURL: null,
     },
-  ]);
+  });
 
   const onLogout = () => {
     authService.logout();
@@ -56,16 +56,32 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrUpdateCard = (card) => {
+    // 예전의 상태인 cards를 받아서 복사해와서, card안에있는 해당하는 키를 업데이트해준다
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
   };
 
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
+  };
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
